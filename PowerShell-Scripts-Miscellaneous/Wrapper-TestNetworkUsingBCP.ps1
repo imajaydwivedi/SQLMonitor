12234d275af9e5b117a6d110675ed275365e6861
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-09-05 10:46:10 +0530

    Updated network testing script for parallel disks

--- PowerShell-Scripts-Miscellaneous/Wrapper-TestNetworkUsingBCP.ps1
+++ PowerShell-Scripts-Miscellaneous/Wrapper-TestNetworkUsingBCP.ps1
@@ -7,7 +7,7 @@ Param (
     [Parameter(Mandatory=$false)]
     [int]$Threads = 2,
     [Parameter(Mandatory=$false)]
-    [String]$DestinationDirectory = "D:\",
+    [String[]]$DestinationDirectory = @("D:\"),
     [Parameter(Mandatory=$false)]
     [int]$DaysThreshold = 0,
     [Parameter(Mandatory=$false)]
@@ -34,29 +34,34 @@ if([String]::IsNullOrEmpty($CollectionTime)) {
 }
 $CollectionTimeString = $(Get-Date -Format yyyyMMMdd_HHmm)
 
-
 # Add backslash
-if(-not $DestinationDirectory.EndsWith('\')) {
-    $DestinationDirectory = "$DestinationDirectory\"
+[System.Collections.ArrayList]$newDestinationDirectory = @()
+foreach($dd in $DestinationDirectory)
+{
+    $dir = $dd
+    if(-not $dd.EndsWith('\')) {
+        $dir = "$dd\"
+    }
+
+    # If a drive letter, then add folder name automatically
+    if($dd.Length -le 3) {
+        $dir = "$($dd)DBA-Network-Test\"
+    }
+
+    $newDestinationDirectory.Add($dir) | Out-Null
 }
 
-# If a drive letter, then add folder name automatically
-if($DestinationDirectory.Length -le 3) {
-    $DestinationDirectory = "$($DestinationDirectory)DBA-Network-Test\"
-}
+# Save all Batch files & Result files in 1st directory
+$firstDestinationDirectory = $newDestinationDirectory[0]
 
-$dataFilesDirectory = "$($DestinationDirectory)DataFiles"
-$batchFilesDirectory = "$($DestinationDirectory)BatchFiles"
-$resultsDirectory = "$($DestinationDirectory)Results"
+[System.Collections.ArrayList]$dataFilesDirectory = @()
+$batchFilesDirectory = "$($firstDestinationDirectory)BatchFiles"
+$resultsDirectory = "$($firstDestinationDirectory)Results"
+$ExecutionLogFilePath = "$($firstDestinationDirectory)$ExecutionLogFile"
 
 # Check batchFilesDirectory folder
 if(-not (Test-Path $batchFilesDirectory)) {
     New-Item $batchFilesDirectory -ItemType Directory -Force | Out-Null
-}
-
-# Check dataFilesDirectory folder
-if(-not (Test-Path $dataFilesDirectory)) {
-    New-Item $dataFilesDirectory -ItemType Directory -Force | Out-Null
 }
 
 # Check resultsDirectory folder
@@ -64,20 +69,33 @@ if(-not (Test-Path $resultsDirectory)) {
     New-Item $resultsDirectory -ItemType Directory -Force | Out-Null
 }
 
+# Check Log File existence
+if(-not(Test-Path $ExecutionLogFilePath)) {
+    New-Item -Path $ExecutionLogFilePath -ItemType File | Out-Null
+}
+
+# Create all data directories if not exists
+foreach($dd in $newDestinationDirectory)
+{
+    $dir = "$($dd)DataFiles"
+    $dataFilesDirectory.Add($dir) | Out-Null
+
+    if(-not (Test-Path $dir)) {
+        New-Item $dir -ItemType Directory -Force | Out-Null
+    }
+}
+
 # Cleanup old files 
 if($CleanupFilesBeforeActivity) {
     "$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'INFO:', "Removing old files.." | Write-Verbose
     Get-ChildItem -Path $batchFilesDirectory -Include *.* -File -Recurse | foreach { $_.Delete()} | Out-Null
-    Get-ChildItem -Path $dataFilesDirectory -Include *.* -File -Recurse | foreach { $_.Delete()} | Out-Null
     Get-ChildItem -Path $resultsDirectory -Include *.* -File -Recurse | foreach { $_.Delete()} | Out-Null
+
+    foreach($dfd in $dataFilesDirectory) {
+        Get-ChildItem -Path $dfd -Include *.* -File -Recurse | foreach { $_.Delete()} | Out-Null
+    }
 }
 
-$ExecutionLogFilePath = "$($DestinationDirectory)$ExecutionLogFile"
-
-# Check Log File existence
-if(-not(Test-Path $ExecutionLogFilePath)) {
-    New-Item -Path $ExecutionLogFile -ItemType File | Out-Null
-}
 
 "$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'START:', "Starting Networking Test.." | Tee-Object $ExecutionLogFilePath -Append | Write-Host -ForegroundColor Yellow
 $PSBoundParameters | Tee-Object $ExecutionLogFilePath -Append
@@ -89,20 +107,33 @@ if([String]::IsNullOrEmpty($SQLUser) -or [String]::IsNullOrEmpty($SQLUserPasswor
 
 # Loop, and generate batch files
 [int]$counter = 1
-[System.Collections.ArrayList]$BatchProcesses = @()
-[System.Collections.ArrayList]$ResultFiles = @()
-while ($counter -le $Threads) {
-    $outputDataFile = "$dataFilesDirectory\DBA_BCP_Test_$counter.dat"
+[System.Collections.ArrayList]$batchProcesses = @()
+[System.Collections.ArrayList]$resultFiles = @()
+[System.Collections.ArrayList]$dataFiles = @()
+$dataDirectoryCount = $dataFilesDirectory.Count
+
+while ($counter -le $Threads) 
+{
+    if($counter -le $dataDirectoryCount) {
+        $indexValue = $counter-1
+    }
+    else {
+        $indexValue = ($counter-1)%$dataDirectoryCount
+    }
+
+    $outputDataFile = "$($dataFilesDirectory[$indexValue])\DBA_BCP_Test_$counter.dat"        
+    $dataFiles.Add($outputDataFile) | Out-Null
+    
     $outputBatchFile = "$batchFilesDirectory\DBA_BCP_Test_Batch_$counter.bat"
     $outputLogFile = "$resultsDirectory\$($CollectionTimeString)__DBA_BCP_Test_Batch_$counter`__Result.txt"
 
     $ResultFiles.Add($outputLogFile) | Out-Null
     
     if($DaysThreshold -eq 0) {
-        $scriptCode = "BCP `"select * from dbo.vw_performance_counters pc where pc.collection_time_utc between dateadd(hour,-1,getutcdate()) and getutcdate()`" "
+        $scriptCode = "BCP `"select * from tempdb..performance_counters_dummy pc where pc.collection_time_utc between dateadd(hour,-1,getutcdate()) and getutcdate()`" "
     }
     else {
-        $scriptCode = "BCP `"select * from dbo.vw_performance_counters pc where pc.collection_time_utc between dateadd(day,-$($DaysThreshold+1),getutcdate()) and dateadd(day,-1,getutcdate())`" "
+        $scriptCode = "BCP `"select * from tempdb..performance_counters_dummy pc where pc.collection_time_utc between dateadd(day,-$($DaysThreshold+1),getutcdate()) and dateadd(day,-1,getutcdate())`" "
     }
     $scriptCode = $scriptCode +" queryout `"$outputDataFile`" -S $DataSourceServer -d $DataSourceDb -o $outputLogFile "
     if(-not [String]::IsNullOrEmpty($SQLUserPassword)) {
@@ -143,11 +174,20 @@ while ($counter -le $Threads) {
 }
 
 if($ExecuteSerially -eq $false) {
-    $BatchProcesses | Wait-Job | Out-Null
-    #$BatchProcesses | Get-Job | Out-Null
+    $batchProcesses | Wait-Job | Out-Null
+
     $batchProcessResult = @()
-    $batchProcessResult += $BatchProcesses | Receive-Job | Out-Null
-    $BatchProcesses | Remove-Job | Out-Null
+    $batchProcessResult += $batchProcesses | Receive-Job
+
+    $failedJobsCount = 0
+    $failedJobsCount = ($batchProcesses | Where-Object {$_.State -eq 'Failed'}).Count
+
+    if($failedJobsCount -gt 0) {
+        "`n$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'ERROR:', "$failedJobsCount batches failed." | Tee-Object $ExecutionLogFilePath -Append | Write-Host -ForegroundColor Red
+        ($batchProcesses | Where-Object {$_.State -eq 'Failed'}).JobStateInfo.Reason
+    }
+    
+    $batchProcesses | Remove-Job | Out-Null
 }
 
 $EndTime = Get-Date
@@ -156,7 +196,7 @@ $elapsedTime = New-TimeSpan -Start $startTime -End $EndTime
 $elapsedTimeSeconds = $elapsedTime.TotalSeconds
 
 
-# Loop, and generate batch files
+# Loop through result files, and extract info
 [int]$counter = 1
 $totalSizeMBFiles = 0
 $totalSizeBytesFiles = 0
@@ -170,8 +210,10 @@ while ($counter -le $Threads)
     $timeSeconds = 0
     $packetSizeBytes = 0
     $countPackets = 0
+    $indexValue = $counter-1
 
-    $outputDataFile = "$dataFilesDirectory\DBA_BCP_Test_$counter.dat"
+    #$outputDataFile = "$dataFilesDirectory\DBA_BCP_Test_$counter.dat"
+    $outputDataFile = $dataFiles[$indexValue]
     $outputBatchFile = "$batchFilesDirectory\DBA_BCP_Test_Batch_$counter.bat"
     $outputLogFile = "$resultsDirectory\$($CollectionTimeString)__DBA_BCP_Test_Batch_$counter`__Result.txt"
 
@@ -231,9 +273,10 @@ $latencyMS = [math]::Ceiling($TotalNetworkTimeMS/$packetsTotal)
 
 <#
 
+cls
 D:\GitHub-Personal\SQLMonitor\PowerShell-Scripts-Miscellaneous\Wrapper-TestNetworkUsingBCP.ps1 `
-        -DataSourceServer OfficeLaptop -DataSourceDb DBA -DestinationDirectory "D:\" `
-        -Threads 2 -ExecuteGeneratedBatchFiles $true -ExecuteSerially $false -DaysThreshold 1 `
-        -SQLUser grafana -SQLUserPassword grafana
+        -DataSourceServer OfficeLaptop -DataSourceDb DBA -DestinationDirectory @("C:\BCP\","D:\BCP","E:\BCP\") `
+        -Threads 5 -ExecuteGeneratedBatchFiles $true -ExecuteSerially $false -DaysThreshold 1 `
+        -SQLUser grafana -SQLUserPassword grafana -Debug
 
 #>
