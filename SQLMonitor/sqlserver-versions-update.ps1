671c3ce5b8b8ae616b00c6f5bc5c2f8271a0de3a
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2025-02-17 06:54:52 +0530

    #33 Make changes to support sqlexpress as inventory

--- SQLMonitor/sqlserver-versions-update.ps1
+++ SQLMonitor/sqlserver-versions-update.ps1
@@ -16,12 +16,22 @@ Import-Module dbatools;
 $ErrorActionPreference = 'Stop'
 $currentTime = Get-Date
 
+$verbose = $false;
+if ($PSBoundParameters.ContainsKey('Verbose')) { # Command line specifies -Verbose[:$false]
+    $verbose = $PSBoundParameters.Get_Item('Verbose')
+}
+
+$debug = $false;
+if ($PSBoundParameters.ContainsKey('Debug')) { # Command line specifies -Debug[:$false]
+    $debug = $PSBoundParameters.Get_Item('Debug')
+}
+
 "$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'INFO:', "Fetch file from Internet.."
 $SqlServerVersionsQuery = (Invoke-WebRequest $GitHubURL -UseBasicParsing).Content
 
-"$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'INFO:', "Execute query against [$SqlInstance].[$Database].$TableName.."
+"$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'INFO:', "Execute query against [$SqlInstance].[$Database].SqlServerVersions.."
 $sqlInstanceObj = Connect-DbaInstance -SqlInstance $SqlInstance -ClientName "(dba) Update-SqlServerVersions" -TrustServerCertificate -EncryptConnection -ErrorAction Stop
-$sqlInstanceObj | Invoke-DbaQuery -Database $Database -Query $SqlServerVersionsQuery -EnableException;
+$sqlInstanceObj | Invoke-DbaQuery -Database $Database -Query $SqlServerVersionsQuery -EnableException -MessagesToOutput:$verbose;
 
-
+"$(Get-Date -Format yyyyMMMdd_HHmm) {0,-10} {1}" -f 'SUCCESS:', "Script completed successfully."
 
